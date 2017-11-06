9fda362f942c740395db6c74fb74884eccc82ecc
igor <i.matukin@gmail.com>
2017-11-06 21:23:04 +0400

    bluetooth

--- macOS/bluetooth/restart_bluetooth.sh
+++ macOS/bluetooth/restart_bluetooth.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-sudo kextunload -b com.apple.iokit.BroadcomBluetoothHostControllerUSBTransport
-sudo kextload -b com.apple.iokit.BroadcomBluetoothHostControllerUSBTransport
+sudo launchctl stop com.apple.blued
+sudo launchctl start com.apple.blued
+
+# a brute-force method
+# sudo pkill blued
+
+# an older OS X version
+# sudo launchctl unload /System/Library/LaunchDaemons/com.apple.blued.plist
+# sudo launchctl load /System/Library/LaunchDaemons/com.apple.blued.plist
+# or
+# sudo kextunload -b com.apple.iokit.BroadcomBluetoothHostControllerUSBTransport
+# sudo kextload -b com.apple.iokit.BroadcomBluetoothHostControllerUSBTransport
