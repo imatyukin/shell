c2c36351b9941b8b1bef1b0dfac9407153a50d4c
igor <i.matukin@gmail.com>
2017-12-17 16:49:34 +0400

    textutil

---
+++ macOS/textutil/textutil.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Конвертация текстовых файлов
+
+# Конвертация в файл Word
+# (DOCX, DOC, RTF, HTML, ODT или WebArchive (веб-страница в формате Safari))
+# textutil -convert txt <файл.docx>
+
+# Массовая конвертация файлов
+# (в любом направлении, без сохранения форматирования)
+# textutil -convert txt ~/Desktop/*.docx
